c7b507141f51474df8eb3159ff82cb3057efb1f6
Austin Bingham <austin@sixty-north.com>
2017-03-27 21:25:34 +0200

    Added setup.py and updated license.

---
+++ setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+with open('README.md') as f:
+    readme = f.read()
+
+setup(
+    name='elm_kernel',
+    version='0.1',
+    packages=['elm_kernel'],
+    description='Jupyter kernel for executing Elm code',
+    long_description=readme,
+    author='Austin Bingham',
+    author_email='austin@sixty-north.com',
+    url='https://github.com/abingham/jupyter-elm-kernel',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+)
